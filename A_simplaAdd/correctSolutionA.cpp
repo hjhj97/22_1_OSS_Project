9a85c61773552a14ecd2d12989e79e4614b93a58
hjhjaueon97@naver.com <hjhjaueon97@naver.com>
2022-06-23 15:25:55 +0900

    add A

---
+++ A_simplaAdd/correctSolutionA.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  scanf("%d %d",&a,&b);
+  long long sum = a + b;
+  printf("%lld\n",sum);
+}
---
+++ A_simplaAdd/dataGenA.cpp
@@ -0,0 +1,14 @@
+#include "testlib.h"
+using namespace std;
+ 
+int main(int argc, char* argv[]) {
+  registerGen(argc,argv,1);
+  
+  // int RANGE = atoi(argv[1]);
+  // int a = rnd.next(RANGE);
+  // int b = rnd.next(RANGE);
+  int a=1,b=1;
+  printf("%d %d\n",a,b);
+ 
+  return 0;
+}
